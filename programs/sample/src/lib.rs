f580b5c64670f1ba00429ce99ddd1ea3a987d78b
metaproph3t <metaproph3t@protonmail.com>
2023-12-24 00:00:00 +0000

    Create demo repo

---
+++ programs/sample/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("7WEsVmbRMFMJ5TXBuauiQRA7BLU7sgGj6ytZLdShvVme");
+
+#[program]
+pub mod sample {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
