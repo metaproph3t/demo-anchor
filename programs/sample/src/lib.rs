e7e5c82fa2d4370675a814ba5c370f918fbfcba8
metaproph3t <metaproph3t@protonmail.com>
2023-12-24 00:00:00 +0000

    Make smol change

--- programs/sample/src/lib.rs
+++ programs/sample/src/lib.rs
@@ -6,6 +6,7 @@ declare_id!("7WEsVmbRMFMJ5TXBuauiQRA7BLU7sgGj6ytZLdShvVme");
 pub mod sample {
     use super::*;
 
+
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
